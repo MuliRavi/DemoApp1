4d6139301c6b62b5124a83fb8a72846b0c96c113
muli.2141702 <muli.2141702@HYDPCM570060L.ad.infosys.com>
2022-10-09 15:35:38 +0530

    This is my First Commit Muli2141702@HYDPCM570060L.ad.infosys.com

---
+++ DemoApp1/src/test/java/com/inf/demo/DemoApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
